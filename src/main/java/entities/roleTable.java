1f63210fd05e9b82602a5fd9595b175c150afde5
Rodolfo Samqui <rasamqui@correo.url.edu.gt>
2024-04-28 17:29:49 -0600

    Database connection and entities
    User module entities and database connection class programmed

---
+++ src/main/java/entities/roleTable.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class tblRol {
+    private int roleId;
+    private String name;
+    private String description;
+
+    public tblRol() {
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
