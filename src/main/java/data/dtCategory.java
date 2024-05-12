3eb27da2b3fe652c2f6d295affcbca4a7ae53838
Rodolfo Samqui <rasamqui@correo.url.edu.gt>
2024-05-12 12:19:17 -0600

    Added dtCategory class

--- src/main/java/data/dtCategory.java
+++ src/main/java/data/dtCategory.java
@@ -1,2 +1,67 @@
-package main.java.data;public class dtCategory {
+package main.java.data;
+
+import entities.categoryTable;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class dtCategory {
+
+    data.connectionPool pc = data.connectionPool.getInstance();
+    Connection c = null;
+    private ResultSet rsCategory = null;
+    private ResultSet rs = null;
+    private PreparedStatement ps = null;
+
+    public dtCategory() {
+    }
+
+    public void fillRsCategory(Connection c) {
+        try{
+            this.ps = c.prepareStatement("SELECT * FROM sibet.category;", ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE, ResultSet.HOLD_CURSORS_OVER_COMMIT);
+            this.rsCategory = ps.executeQuery();
+        }catch (Exception e){
+            System.out.println("DATOS: ERROR EN LISTAR CATEGORIAS " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<categoryTable> listCategories(){
+        ArrayList<categoryTable> listCat = new ArrayList<categoryTable>();
+        try {
+            this.c = pc.getConnection();
+            this.ps = this.c.prepareStatement("SELECT * FROM sibet.category;", ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            this.rsCategory = this.ps.executeQuery();
+
+            while (this.rsCategory.next()) {
+                categoryTable cat = new categoryTable();
+                cat.setCategoryId(this.rsCategory.getInt("categoryId"));
+                cat.setName(this.rsCategory.getString("name"));
+                cat.setDescription(this.rsCategory.getString("description"));
+                listCat.add(cat);
+            }
+        } catch (Exception e) {
+            System.out.println("DATOS: ERROR EN LISTAR CATEGORIAS " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            try {
+                if (this.rsCategory != null) {
+                    this.rsCategory.close();
+                }
+                if (this.ps != null) {
+                    this.ps.close();
+                }
+                if (this.c != null) {
+                    this.c.close();
+                }
+            } catch (Exception e) {
+                System.out.println("DATOS: ERROR EN LISTAR CATEGORIAS " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+        return listCat;
+    }
+
 }
