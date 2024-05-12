f0cc0f4d7ac455206addf31f8e94f03d3d636d84
Rodolfo Samqui <rasamqui@correo.url.edu.gt>
2024-05-11 19:56:05 -0600

    Entities created

---
+++ src/main/java/entities/typeTable.java
@@ -0,0 +1,26 @@
+package entities;
+
+public class typeTable {
+    private int typeId;
+    private String name;
+
+    public typeTable() {
+    }
+
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(int typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
